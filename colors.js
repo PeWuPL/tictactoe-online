37f5dc9f087a471937e69595953fde304082923e
PeWu <42903478+PeWuPL@users.noreply.github.com>
2023-01-19 22:47:03 +0100

    missing lib upload

---
+++ colors.js
@@ -0,0 +1,45 @@
+export const f = {
+	reset: 	"\u001b[0m",
+	white: 	"\u001b[37m",
+	cyan: 	"\u001b[36m",
+	magenta:"\u001b[35m",
+	blue: 	"\u001b[34m",
+	yellow: "\u001b[33m",
+	black: 	"\u001b[30m",
+	red: 	"\u001b[31m",
+	green: 	"\u001b[32m",
+
+	lwhite: 	"\u001b[37;1m",
+	lcyan: 		"\u001b[36;1m",
+	lmagenta:	"\u001b[35;1m",
+	lblue: 		"\u001b[34;1m",
+	lyellow: 	"\u001b[33;1m",
+	lblack: 	"\u001b[30;1m",
+	lred: 		"\u001b[31;1m",
+	lgreen: 	"\u001b[32;1m"
+};
+export const b = {
+	reset: 	"\u001b[0m",
+	white: 	"\u001b[47m",
+	cyan: 	"\u001b[46m",
+	magenta:"\u001b[45m",
+	blue: 	"\u001b[44m",
+	yellow: "\u001b[43m",
+	green: 	"\u001b[42m",
+	red: 	"\u001b[41m",
+	black: 	"\u001b[40m",
+
+	lwhite: 	"\u001b[47;1m",
+	lcyan: 		"\u001b[46;1m",
+	lmagenta:	"\u001b[45;1m",
+	lblue: 		"\u001b[44;1m",
+	lyellow: 	"\u001b[43;1m",
+	lgreen: 	"\u001b[42;1m",
+	lred: 		"\u001b[41;1m",
+	lblack: 	"\u001b[40;1m"
+
+};
+export const s = {
+	hide: "\u001b[?25l",
+	show: "\u001b[?25h"
+};
